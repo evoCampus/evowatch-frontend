2096310c124ce95e8695833227eba0b256ad9032
Puskás Marcell <puskasmarcell05@gmail.com>
2024-12-25 15:34:45 +0100

    Created basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import { ApiClient } from './app/apiClient.ts'
+import MainPage from "./pages/mainPage/MainPage";
 
 function App() {
-  const [userName, setUserName] = useState("");
-
-
-  console.log(userName);
-
-  const addUser = () => {
-    apiClient.userService.addUser({
-      normalName: userName,
-      email: "test",
-      nickname: userName,
-      password: "string",
-      imageUrl: "http://localhost:5092"
-    });
-  }
-
-  let apiClient = new ApiClient();
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-red-300">evoWatch</h1>
-      <div className="card">
-
-        <input type='text' value={userName} onChange={(e) => {setUserName(e.target.value)} }></input>
-
-        <button onClick={addUser}>
-          Register
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <header className="w-full h-20 border-b-2">Header placeholder</header>
+            <div className="flex h-full">
+                <div className="h-full w-64 border-r-2">Sidebar placeholder</div>
+                <main className="h-full w-full">Mainpage placeholder</main>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
