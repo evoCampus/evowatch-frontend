7b832a51794e188da695a8930ee192c9e36cabbd
Puskás Marcell <puskasmarcell05@gmail.com>
2025-01-12 20:54:27 +0100

    Increased repeating filler contetnt for testing

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -2,14 +2,22 @@ import Card from "./Card";
 import CategoryTitle from "./CategoryTitle";
 
 export default function MainPage() {
-    return (
-        <div className="p-5 flex flex-col gap-8">
-            <CategoryTitle title="Classics" />
-            <div className="flex flex-wrap gap-4">
-                <Card title="Back to the Future" link="" imgUrl="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQiAUI52_-niASQabl_PqdkJQ-moVziM6ahfiYkZW9NSnhcciNw" />
-                <Card title="Back to the Future" link="" imgUrl="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQiAUI52_-niASQabl_PqdkJQ-moVziM6ahfiYkZW9NSnhcciNw" />
-                <Card title="Back to the Future" link="" imgUrl="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQiAUI52_-niASQabl_PqdkJQ-moVziM6ahfiYkZW9NSnhcciNw" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="p-5 flex flex-col gap-8">
+      {[...Array(15)].map((_, i) => (
+        <>
+          <CategoryTitle title="Classics" />
+          <div className="flex flex-wrap gap-4">
+            {[...Array(15)].map((_, i) => (
+              <Card
+                title="Back to the Future"
+                link=""
+                imgUrl="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQiAUI52_-niASQabl_PqdkJQ-moVziM6ahfiYkZW9NSnhcciNw"
+              />
+            ))}
+          </div>
+        </>
+      ))}
+    </div>
+  );
 }
