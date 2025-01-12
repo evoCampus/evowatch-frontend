f91d4f710684f6204fab2201d5e8151eecaaabc8
bence <67009276+bqnce@users.noreply.github.com>
2025-01-12 18:37:31 +0100

    Sidebarbutton component have the icon's size, strokeWidth and color props

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -14,61 +14,78 @@ import SidebarButton from "./SidebarButton";
 const Sidebar = () => {
   return (
     <>
-      <div className="bg-[#070707] text-white">
+      <div className="bg-[#070707] text-[#dbdbdb]">
         <div className="border-r border-[#1f1f1f] w-[13%] h-screen">
           <SidebarSection title="Explore">
-            <SidebarButton title="Home" url="/" icon={<House size={20} />} />
+            <SidebarButton title="Home" url="/" size={20} icon={<House />} />
             <SidebarButton
               title="Movies"
               url="/"
-              icon={<Clapperboard size={20} />}
+              size={20}
+              icon={<Clapperboard />}
             />
             <SidebarButton
               title="Series"
               url="/"
-              icon={<Popcorn size={20} />}
+              size={20}
+              icon={<Popcorn />}
             />
           </SidebarSection>
-
           <SidebarSection title="My content">
-            <SidebarButton title="Watchlist" url="/" icon={<Eye size={20} />} />
+            <SidebarButton title="Watchlist" url="/" size={20} icon={<Eye />} />
             <SidebarButton
               title="Favorites"
               url="/"
-              icon={<Star size={20} />}
+              size={20}
+              icon={<Star />}
             />
             <SidebarButton
               title="Recommended"
               url="/"
-              icon={<Workflow size={20} />}
+              size={20}
+              icon={<Workflow />}
             />
           </SidebarSection>
-
           <SidebarSection title="Categories">
             <SidebarButton
               title="Action"
               url="/"
-              icon={<Dot size={20} color="#3498db" strokeWidth={8} />}
+              size={20}
+              color="#3498db"
+              strokeWidth={8}
+              icon={<Dot />}
             />
             <SidebarButton
               title="Sci-fi"
               url="/"
-              icon={<Dot size={20} color="#1abc9c" strokeWidth={8} />}
+              size={20}
+              color="#1abc9c"
+              strokeWidth={8}
+              icon={<Dot />}
             />
             <SidebarButton
               title="Horror"
               url="/"
-              icon={<Dot size={20} color="#9b59b6" strokeWidth={8} />}
+              size={20}
+              color="#9b59b6"
+              strokeWidth={8}
+              icon={<Dot />}
             />
             <SidebarButton
               title="Drama"
               url="/"
-              icon={<Dot size={20} color="#f1c40f" strokeWidth={8} />}
+              size={20}
+              color="#f1c40f"
+              strokeWidth={8}
+              icon={<Dot size={20} />}
             />
             <SidebarButton
               title="Comedy"
               url="/"
-              icon={<Dot size={20} color="#e74c3c" strokeWidth={8} />}
+              size={20}
+              color="#e74c3c"
+              strokeWidth={8}
+              icon={<Dot />}
             />
           </SidebarSection>
         </div>
