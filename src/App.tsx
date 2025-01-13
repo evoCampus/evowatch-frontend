f5f48cba3b9dab6d26037aa957a4d98690f399a1
Puskás Marcell <puskasmarcell05@gmail.com>
2025-01-13 05:24:11 +0100

    Requested changes #1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MainPage from "./pages/mainPage/MainPage";
 function App() {
     return (
         <>
-            <header className="w-full min-h-20 border-b-2">Header placeholder</header>
+            <header className="w-full min-h-20 border-b-2 sticky top-0 bg-white">Header placeholder</header>
             <div className="flex h-full">
                 <div className="h-full min-w-64 border-r-2">Sidebar placeholder</div>
                 <main className="h-full flex-grow"><MainPage /></main>
--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -5,18 +5,19 @@ export default function MainPage() {
   return (
     <div className="p-5 flex flex-col gap-8">
       {[...Array(15)].map((_, i) => (
-        <>
+        <div key={i} className="flex flex-col gap-8">
           <CategoryTitle title="Classics" />
           <div className="flex flex-wrap gap-4">
-            {[...Array(15)].map((_, i) => (
+            {[...Array(15)].map((_, j) => (
               <Card
+                key={j}
                 title="Back to the Future"
                 link=""
                 imgUrl="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQiAUI52_-niASQabl_PqdkJQ-moVziM6ahfiYkZW9NSnhcciNw"
               />
             ))}
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
