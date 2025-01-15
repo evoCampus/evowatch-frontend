728ce67e7e9af18a10d1b5f42e5bd8e85a72727e
Puskás Marcell <puskasmarcell05@gmail.com>
2025-01-15 01:35:48 +0100

    Made color theme aligned with @bqnce's sidebar and topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import MainPage from "./pages/mainPage/MainPage";
 
 function App() {
-    return (
-        <>
-            <header className="w-full min-h-20 border-b-2 sticky top-0 bg-white">Header placeholder</header>
-            <div className="flex h-full">
-                <div className="h-full min-w-64 border-r-2">Sidebar placeholder</div>
-                <main className="h-full flex-grow"><MainPage /></main>
-            </div>
-        </>
-    );
+  return (
+    <div className="flex min-h-screen flex-col bg-[#070707] text-[#dbdbdb]">
+      <header className="sticky top-0 min-h-20 w-full border-b-2 border-[#1f1f1f] bg-[#070707]">
+        Header placeholder
+      </header>
+      <div className="flex flex-grow">
+        <div className="min-w-64 border-r-2 border-[#1f1f1f]">
+          Sidebar placeholder
+        </div>
+        <main className="flex-grow">
+          <MainPage />
+        </main>
+      </div>
+    </div>
+  );
 }
 export default App;
--- src/pages/mainPage/Card.tsx
+++ src/pages/mainPage/Card.tsx
@@ -7,9 +7,9 @@ interface CardProps {
 export default function Card(props : CardProps) {
 
     return (
-        <div className="px-2 w-1/2 md:w-1/4 xl:w-1/6 2xl:w-[12.5%] min-[2020px]:w-[215px]">
+        <div className="px-4 py-6 w-1/2 md:w-1/4 xl:w-1/6 2xl:w-[12.5%] min-[2020px]:w-[215px]">
             <a href={props.link}>
-                <img src={props.imgUrl} className="w-full rounded-3xl border-2 border-black" />
+                <img src={props.imgUrl} className="w-full rounded-3xl border-2 border-[#2f2f2f]" />
                 <p className="text-xl font-semibold text-center hover:underline">{props.title}</p>
             </a>
         </div>
