3fe059eff05095c60447d8c31fea6f566920d4a4
Puskás Marcell <puskasmarcell05@gmail.com>
2025-01-15 01:40:36 +0100

    Card hover scale animation

--- src/pages/mainPage/Card.tsx
+++ src/pages/mainPage/Card.tsx
@@ -9,8 +9,8 @@ export default function Card(props : CardProps) {
     return (
         <div className="px-4 py-6 w-1/2 md:w-1/4 xl:w-1/6 2xl:w-[12.5%] min-[2020px]:w-[215px]">
             <a href={props.link}>
-                <img src={props.imgUrl} className="w-full rounded-3xl border-2 border-[#2f2f2f]" />
-                <p className="text-xl font-semibold text-center hover:underline">{props.title}</p>
+                <img src={props.imgUrl} className="w-full rounded-3xl border-2 border-[#2f2f2f] hover:scale-125 transition-all duration-300 peer z-50" />
+                <p className="text-xl font-semibold text-center hover:underline peer-hover:opacity-0 transition-opacity duration-200">{props.title}</p>
             </a>
         </div>
     );
