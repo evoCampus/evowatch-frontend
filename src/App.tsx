d597c21cc6e1f08ddf10e81bbb250ff75f5f6a2c
Puskás Marcell <puskasmarcell05@gmail.com>
2025-01-16 18:21:58 +0100

    Fixed Z-index of header and card relative to each other

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MainPage from "./pages/mainPage/MainPage";
 function App() {
   return (
     <div className="flex min-h-screen flex-col bg-[#070707] text-[#dbdbdb]">
-      <header className="sticky top-0 min-h-20 w-full border-b-2 border-[#1f1f1f] bg-[#070707]">
+      <header className="sticky top-0 min-h-20 w-full border-b-2 border-[#1f1f1f] bg-[#070707] z-30">
         Header placeholder
       </header>
       <div className="flex flex-grow">
